1468b7fb46167540ee5200f761b1dbd9ff474f27
hezf <287645587@qq.com>
2020-08-28 09:59:02 +0800

    docs: 更新

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'vue-page-stack-example' : '/',
+  publicPath: '/',
   assetsDir: 'static',
   productionSourceMap: false,
   outputDir: 'docs',
